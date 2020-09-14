42c57dcacb882151bc4d46617fc043c492a16eab
Trayan Iliev <office@iproduct.org>
2020-09-14 15:18:47 +0300

    genric repo interfaces added

---
+++ 01-01-invoicing/src/org/iproduct/invoicing/dao/Repository.java
@@ -0,0 +1,15 @@
+package org.iproduct.invoicing.dao;
+
+import org.iproduct.invoicing.model.Product;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface Repository<K, E extends Identifiable<K>> {
+    Collection<E> findAll();
+    E findById(K id);
+    E create(E item);
+    E update(E item);
+    E deleteById(K id);
+    long size();
+}
