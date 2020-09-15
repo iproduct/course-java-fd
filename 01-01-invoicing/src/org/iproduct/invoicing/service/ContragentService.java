d9fbe769e50438c405bd0ff1c5aa443a9f3548d4
Trayan Iliev <office@iproduct.org>
2020-09-15 15:24:23 +0300

    ProductService and implementation

---
+++ 01-01-invoicing/src/org/iproduct/invoicing/service/ContragentService.java
@@ -0,0 +1,15 @@
+package org.iproduct.invoicing.service;
+
+import org.iproduct.invoicing.exceptions.NonexistingEntityException;
+import org.iproduct.invoicing.model.Contragent;
+
+import java.util.List;
+
+public interface ContragentService {
+    List<Contragent> getAllContragents();
+    Contragent getContragentById(Long id);
+    Contragent addContragent(Contragent contragent);
+    Contragent updateContragent(Contragent contragent) throws NonexistingEntityException;
+    Contragent deleteContragentById(Long id) throws NonexistingEntityException;
+    long size();
+}
