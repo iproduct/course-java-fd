0c57f7a72f54d3f6fc697db3f1b019defb084f21
Trayan Iliev <office@iproduct.org>
2020-09-17 17:14:26 +0300

    inputDouble completed

--- 01-01-invoicing/src/org/iproduct/invoicing/view/InputUtils.java
+++ 01-01-invoicing/src/org/iproduct/invoicing/view/InputUtils.java
@@ -13,10 +13,9 @@ public class InputUtils {
         for (FieldConfig fc : fieldConfigs) {
             Object result = null;
             switch (fc.type) {
-//                case INTEGER: result = inputInteger(fc);
-//                case DECIMAL: result = inputDecimal(fc);
-                case STRING:
-                    result = inputString(fc);
+                case INTEGER: result = inputInteger(fc); break;
+                case DECIMAL: result = inputDouble(fc); break;
+                case STRING: result = inputString(fc); break;
 //                case DATE: result = inputDate(fc);
             }
             System.out.println("You entered: " + result);
@@ -52,5 +51,84 @@ public class InputUtils {
             }
         } while (error);
         return answer;
+    }
+
+    public static Integer inputInteger(FieldConfig config) {
+        String answer;
+        boolean error;
+        do {
+            error = false;
+            System.out.printf("Enter %s" + (config.defaultValue != null || config.optional ? " [<Enter> for '%s']": "") + ":",
+                    config.label, config.defaultValue);
+            answer = sc.nextLine(); // read answer from console
+            if (answer.isEmpty()) {
+                if (config.defaultValue != null) {
+                    answer = config.defaultValue;
+                } else if (config.optional) {
+                    return null;
+                } else {
+                    System.out.println("The field is mandatory, please enter a value.");
+                    error = true;
+                }
+            } else { // answer is not empty - validation needed
+                if (config.precision > 0 && answer.length() > config.precision) {
+                    System.out.printf("The maximum number of characters is %s.%n", config.precision);
+                    error = true;
+                }
+                if (config.regex != null && !Pattern.matches(config.regex, answer)) {
+                    System.out.printf("The answer must match%s.%n", config.regex);
+                    error = true;
+                }
+            }
+            try {
+                return Integer.valueOf(answer);
+            } catch (NumberFormatException ex) {
+                System.out.println("The answer should be a valid integer number.");
+                error = true;
+            }
+        } while (error);
+        return null;
+    }
+
+    public static Double inputDouble(FieldConfig config) {
+        String answer;
+        boolean error;
+        Double result = null;
+        do {
+            error = false;
+            System.out.printf("Enter %s" + (config.defaultValue != null || config.optional ? " [<Enter> for '%s']": "") + ":",
+                    config.label, config.defaultValue);
+            answer = sc.nextLine(); // read answer from console
+            if (answer.isEmpty()) {
+                if (config.defaultValue != null) {
+                    answer = config.defaultValue;
+                } else if (config.optional) {
+                    return null;
+                } else {
+                    System.out.println("The field is mandatory, please enter a value.");
+                    error = true;
+                }
+            } else { // answer is not empty - validation needed
+                if (config.precision > 0 && answer.length() > config.precision) {
+                    System.out.printf("The maximum number of characters is %s.%n", config.precision);
+                    error = true;
+                }
+                if (config.regex != null && !Pattern.matches(config.regex, answer)) {
+                    System.out.printf("The answer must match%s.%n", config.regex);
+                    error = true;
+                }
+            }
+            try {
+                result = Double.valueOf(answer);
+                if(config.fraction > 0) {
+                    double power = Math.pow(10, config.fraction);
+                    result = (Math.round(result * power)) / power;
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("The answer should be a valid real number.");
+                error = true;
+            }
+        } while (error);
+        return result;
     }
 }
