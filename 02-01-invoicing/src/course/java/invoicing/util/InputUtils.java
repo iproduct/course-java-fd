90802629ce5aa34252212e9c090287760c2ca1c5
Trayan Iliev <office@iproduct.org>
2020-10-26 12:11:56 +0200

    input methods added to InputUtils

--- 02-01-invoicing/src/course/java/invoicing/util/InputUtils.java
+++ 02-01-invoicing/src/course/java/invoicing/util/InputUtils.java
@@ -1,9 +1,11 @@
 package course.java.invoicing.util;
 
 import course.java.invoicing.exception.InvalidUserDataException;
+import course.java.invoicing.model.Unit;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -32,44 +34,37 @@ public class InputUtils {
                         break;
                     case LONG:
                         longResult = inputLong(fc);
-                        result = (longResult != null) ? longResult.longValue() : null;
-                        method = instance.getClass().getMethod(setterName.toString(), Integer.class);
+                        result = (longResult != null) ? longResult: null;
+                        method = instance.getClass().getMethod(setterName.toString(), Long.class);
                         method.invoke(instance, result);
                         break;
                     case DECIMAL:
                         Double doubleResult = inputDouble(fc);
                         result = (doubleResult != null) ? doubleResult.doubleValue() : null;
+                        method = instance.getClass().getMethod(setterName.toString(), Double.class);
+                        method.invoke(instance, result);
+                        break;
+                    case STRING:
+                        result = inputString(fc);
+                        method = instance.getClass().getMethod(setterName.toString(), String.class);
+                        method.invoke(instance, result);
+                        break;
+                    case UNIT:
+                        Unit unitResult = inputUnit(fc);
+                        result = (unitResult != null) ? unitResult : null;
                         method = instance.getClass().getMethod(setterName.toString(), Integer.class);
                         method.invoke(instance, result);
                         break;
-                    case LONG:
-                        longResult = inputLong(fc);
-                        result = (longResult != null) ? longResult.longValue() : null;
-                        method = instance.getClass().getMethod(setterName.toString(), Integer.class);
-                        method.invoke(instance, result);
-                        break;
-                    case LONG:
-                        longResult = inputLong(fc);
-                        result = (longResult != null) ? longResult.longValue() : null;
-                        method = instance.getClass().getMethod(setterName.toString(), Integer.class);
-                        method.invoke(instance, result);
-                        break;
-                    case LONG:
-                        longResult = inputLong(fc);
-                        result = (longResult != null) ? longResult.longValue() : null;
-                        method = instance.getClass().getMethod(setterName.toString(), Integer.class);
-                        method.invoke(instance, result);
-                        break;
-                    case LONG:
-                        longResult = inputLong(fc);
-                        result = (longResult != null) ? longResult.longValue() : null;
+                    case DATE:
+                        LocalDate dateResult = inputDate(fc);
+                        result = (dateResult != null) ? dateResult : null;
                         method = instance.getClass().getMethod(setterName.toString(), Integer.class);
                         method.invoke(instance, result);
                         break;
                 }
-            } catch (NoSuchMethodException | IllegalAccessException |InvocationTargetException ex) {
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {
                 LOG.log(SEVERE,
-                    String.format("Error accessing property '%s' on instance %s.", fc.property, instance.toString()), ex);
+                        String.format("Error accessing property '%s' on instance %s.", fc.property, instance.toString()), ex);
             }
 
         }
@@ -78,75 +73,83 @@ public class InputUtils {
 
     // Implementation helper methods
     private static Long inputLong(FieldConfig fc) {
-        String answer;
+        String answer = null;
         boolean error;
         do {
             error = false;
-            System.out.printf("Input %s" +
-                            (fc.optional || fc.defaultValue != null ? "[<Enter> for %s]" : "") + ":",
-                    fc.label, fc.defaultValue);
-            answer = sc.nextLine();
-            if (answer.isEmpty()) {
-                if (fc.defaultValue != null) {
-                    answer = fc.defaultValue;
-                } else if (fc.optional) {
-                    return null;
-                } else {
-                    System.out.printf("The fields '%s' is mandatory, please enter a value.%n", fc.label);
-                    error = true;
-                    continue;
-                }
-            } else { //the answer is not empty
-                if (fc.presicion > 0 && answer.length() > fc.presicion) {
-                    System.out.printf("The maximum number of characters is %d.%n", fc.presicion);
-                    error = true;
-                    continue;
-                }
-                if (fc.regex != null && !Pattern.matches(fc.regex, answer)) {
-                    System.out.printf("The answer must match regular expression '%s'.%n", fc.regex);
-                    error = true;
-                    continue;
-                }
-            }
             try {
-                return Long.parseLong(answer);
-            } catch (NumberFormatException ex) {
-                System.out.printf("The answer must be a valid integer number.%n");
+                answer = inputStringOrThrow(fc);
+            } catch (InvalidUserDataException e) {
+                System.out.println(e.getMessage());
                 error = true;
             }
+            if (answer != null) {
+                try {
+                    return Long.parseLong(answer);
+                } catch (NumberFormatException ex) {
+                    System.out.printf("The answer must be a valid integer number.%n");
+                    error = true;
+                }
+            }
         } while (error);
-        return null; //should be never reached
+        return null;
     }
 
     private static Double inputDouble(FieldConfig fc) {
-        String answer;
+        String answer = null;
         boolean error;
         do {
             error = false;
-
             try {
-                double result = Double.parseDouble(answer);
-                if(fc.fraction > 0) {
-                    double powerOfTen = Math.pow(10, fc.fraction);
-                    result = Math.round(result * powerOfTen) / powerOfTen;
-                }
-                return result;
-            } catch (NumberFormatException ex) {
-                System.out.printf("The answer must be a valid integer number.%n");
+                answer = inputStringOrThrow(fc);
+            } catch (InvalidUserDataException e) {
+                System.out.println(e.getMessage());
                 error = true;
             }
+            if (answer != null) {
+                try {
+                    double result = Double.parseDouble(answer);
+                    if (fc.fraction > 0) {
+                        double powerOfTen = Math.pow(10, fc.fraction);
+                        result = Math.round(result * powerOfTen) / powerOfTen;
+                    }
+                    return result;
+                } catch (NumberFormatException ex) {
+                    System.out.printf("The answer must be a valid integer number.%n");
+                    error = true;
+                }
+            }
         } while (error);
-        return null; //should be never reached
+        return null;
     }
 
     private static String inputString(FieldConfig fc) {
-        String answer;
+        String answer = null;
         boolean error;
         do {
             error = false;
-            System.out.printf("Input %s" +
+            try {
+                answer = inputStringOrThrow(fc);
+            } catch (InvalidUserDataException e) {
+                System.out.println(e.getMessage());
+                error = true;
+            }
+        } while (error);
+        return answer;
+    }
+
+    private static Unit inputUnit(FieldConfig fc) {
+        String answer = null;
+        boolean error;
+        StringBuilder enumValues = new StringBuilder();
+        for (Unit val : Unit.values()) {
+            enumValues.append(val.ordinal()).append(") ").append(val);
+        }
+        do {
+            error = false;
+            System.out.printf("Input %s in: %s" +
                             (fc.optional || fc.defaultValue != null ? "[<Enter> for %s]" : "") + ":",
-                    fc.label, fc.defaultValue);
+                    fc.label, enumValues, fc.defaultValue);
             answer = sc.nextLine();
             if (answer.isEmpty()) {
                 if (fc.defaultValue != null) {
@@ -156,36 +159,31 @@ public class InputUtils {
                 } else {
                     System.out.printf("The fields '%s' is mandatory, please enter a value.%n", fc.label);
                     error = true;
-                    continue;
                 }
             } else { //the answer is not empty
                 if (fc.presicion > 0 && answer.length() > fc.presicion) {
                     System.out.printf("The maximum number of characters is %d.%n", fc.presicion);
                     error = true;
-                    continue;
                 }
                 if (fc.regex != null && !Pattern.matches(fc.regex, answer)) {
                     System.out.printf("The answer must match regular expression '%s'.%n", fc.regex);
                     error = true;
-                    continue;
                 }
             }
-            try {
-                double result = Double.parseDouble(answer);
-                if(fc.fraction > 0) {
-                    double powerOfTen = Math.pow(10, fc.fraction);
-                    result = Math.round(result * powerOfTen) / powerOfTen;
+            if (answer != null) {
+                try {
+                    return Unit.values()[Integer.parseInt(answer)];
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+                    System.out.printf("The answer must be a valid integer choice.%n");
+                    error = true;
                 }
-                return result;
-            } catch (NumberFormatException ex) {
-                System.out.printf("The answer must be a valid integer number.%n");
-                error = true;
             }
         } while (error);
-        return null; //should be never reached
+        return null;
     }
 
-    private String inputStringOrThrow (FieldConfig fc) throws InvalidUserDataException {
+
+    private static String inputStringOrThrow(FieldConfig fc) throws InvalidUserDataException {
         String answer;
         System.out.printf("Input %s" +
                         (fc.optional || fc.defaultValue != null ? "[<Enter> for %s]" : "") + ":",
@@ -197,8 +195,8 @@ public class InputUtils {
             } else if (fc.optional) {
                 return null;
             } else {
-               throw new InvalidUserDataException(
-                       String.format("The fields '%s' is mandatory, please enter a value.%n", fc.label));
+                throw new InvalidUserDataException(
+                        String.format("The fields '%s' is mandatory, please enter a value.%n", fc.label));
             }
         } else { //the answer is not empty
             if (fc.presicion > 0 && answer.length() > fc.presicion) {
@@ -211,6 +209,10 @@ public class InputUtils {
             }
         }
         return answer;
+    }
+
+    private static LocalDate inputDate(FieldConfig fc) {
+        return LocalDate.now();
     }
 
 }
