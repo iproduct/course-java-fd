62456e38460ba28aa767313cbc895748f4b345ea
Trayan Iliev <office@iproduct.org>
2021-01-18 12:13:43 +0200

    03-01-invoicing initial commit

---
+++ 03-01-invoicing/src/hello/Hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Date; // Fully qualified class name
+
+public class Hello {
+    private String sayHello(String name) {
+        return "Hi, " + name + " from Java! - " + new Date();
+    }
+
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        String message = hello.sayHello("Trayan") ;
+        System.out.println(message);
+    }
+}
