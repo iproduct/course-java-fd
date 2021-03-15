bc51a9467212528ba479ed757250962e1e0d28cb
Trayan Iliev <office@iproduct.org>
2021-03-15 10:46:41 +0200

    User entity updated to extend AbstractEntity

--- 04-01-invoicing/src/invoicing/model/User.java
+++ 04-01-invoicing/src/invoicing/model/User.java
@@ -1,10 +1,10 @@
 package invoicing.model;
 
 
+import java.util.Date;
 import java.util.Objects;
 
-public class User implements Identifiable<Long>, Comparable<User>{
-    private Long id;
+public class User extends AbstractEntity<Long> implements Comparable<User>{
     private String firstName;
     private String lastName;
     private String username;
@@ -15,10 +15,19 @@ public class User implements Identifiable<Long>, Comparable<User>{
     }
 
     public User(Long id) {
-        this.id = id;
+        super(id);
     }
 
     public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String username, String password,
+                Date created, Date updated, Long createdById, Long updatedById) {
+        super(null, created, updated, createdById, updatedById);
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
@@ -33,15 +42,16 @@ public class User implements Identifiable<Long>, Comparable<User>{
         this.role = role;
     }
 
-    @Override
-    public Long getId() {
-        return id;
+    public User(String firstName, String lastName, String username, String password, Role role,
+                Date created, Date updated, Long createdById, Long updatedById) {
+        super(null, created, updated, createdById, updatedById);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
@@ -84,27 +94,18 @@ public class User implements Identifiable<Long>, Comparable<User>{
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("User{");
-        sb.append("id=").append(id);
+        sb.append("id=").append(getId());
         sb.append(", firstName='").append(firstName).append('\'');
         sb.append(", lastName='").append(lastName).append('\'');
         sb.append(", username='").append(username).append('\'');
         sb.append(", password='").append(password).append('\'');
         sb.append(", role=").append(role);
+        sb.append(", created=").append(getCreated());
+        sb.append(", updated=").append(getUpdated());
+        sb.append(", createdById=").append(getCreatedById());
+        sb.append(", updatedById=").append(getUpdatedById());
         sb.append('}');
         return sb.toString();
     }
