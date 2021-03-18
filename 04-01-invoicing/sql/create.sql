55cce07536add8717a536b923f5a11ff92d7ae5f
Trayan Iliev <office@iproduct.org>
2021-03-18 12:51:27 +0200

    create.sql autoincrement + insert into demo added

--- 04-01-invoicing/sql/create.sql
+++ 04-01-invoicing/sql/create.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.4
 -- Dumped by pg_dump version 13.2
 
--- Started on 2021-03-18 11:51:39
+-- Started on 2021-03-18 12:44:33
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -26,21 +26,36 @@ SET default_table_access_method = heap;
 -- TOC entry 202 (class 1259 OID 49287)
 -- Name: products; Type: TABLE; Schema: public; Owner: postgres
 --
-
+DROP TABLE IF EXISTS public.products;
 CREATE TABLE public.products (
-    id bigint NOT NULL,
-    code character varying(5) NOT NULL,
-    name character varying(80) NOT NULL,
-    description character varying(2048) NOT NULL,
-    price real NOT NULL,
-    unit integer NOT NULL
+     id SERIAL PRIMARY KEY,
+     code character varying(5) NOT NULL,
+     name character varying(80) NOT NULL,
+     description character varying(2048) NOT NULL,
+     price real NOT NULL,
+     unit integer NOT NULL
 );
 
 
 ALTER TABLE public.products OWNER TO postgres;
 
 --
--- TOC entry 2814 (class 0 OID 49287)
+-- TOC entry 203 (class 1259 OID 49295)
+-- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.products ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.products_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 2816 (class 0 OID 49287)
 -- Dependencies: 202
 -- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -50,7 +65,16 @@ COPY public.products (id, code, name, description, price, unit) FROM stdin;
 
 
 --
--- TOC entry 2687 (class 2606 OID 49294)
+-- TOC entry 2823 (class 0 OID 0)
+-- Dependencies: 203
+-- Name: products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.products_id_seq', 1, false);
+
+
+--
+-- TOC entry 2689 (class 2606 OID 49294)
 -- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -58,7 +82,7 @@ ALTER TABLE ONLY public.products
     ADD CONSTRAINT products_pkey PRIMARY KEY (id);
 
 
--- Completed on 2021-03-18 11:51:39
+-- Completed on 2021-03-18 12:44:34
 
 --
 -- PostgreSQL database dump complete
--- 04-01-invoicing/src/jdbcdemo/JdbcDemo.java
+++ 04-01-invoicing/src/jdbcdemo/JdbcDemo.java
@@ -1,7 +1,66 @@
 package jdbcdemo;
 
-public class JdbcDemo {
-    public static void main(String[] args) {
+import invoicing.model.Product;
+import invoicing.model.Unit;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+public class JdbcDemo {
+    public static final List<Product> products = List.of(new Product("BK001", "Thinking in Java",
+                    "Good introduction to Java ...", 35.99),
+            new Product("BK002", "UML Distilled",
+                    "UML described briefly ...", 25.50),
+            new Product("AC001", "Whiteboard Markers", "5 colors set", 7.8),
+            new Product("SV001", "Mobile Internet", "On-demand mobile internet package",
+                    10.99, Unit.GB),
+            new Product("DV001", "2 Band Router", "Supports 2.4 GHz and 5.7 GHz bands",
+                    45.5),
+            new Product("CB001", "Network Cable Cat. 6E", "Gbit Eternet cable UTP",
+                    0.72, Unit.M)
+    );
+
+    public static void main(String[] args) throws IOException {
+        String dbConfigPath = JdbcDemo.class.getClassLoader().getResource("db.properties").getPath();
+        Properties dbProps = new Properties();
+        dbProps.load(new FileInputStream(dbConfigPath));
+
+        // 1. Load DB Driver (optional)
+        try {
+            Class.forName(dbProps.getProperty("driver"));
+        } catch(ClassNotFoundException ex) {
+            System.err.printf("Database driver %s not found.", dbProps.getProperty("driver"));
+            System.exit(0);
+        }
+        System.out.println("Program completed successfully.");
+
+        // 2. Connect to DB
+        try {
+            Connection con = DriverManager.getConnection(dbProps.getProperty("url"), dbProps);
+            System.out.printf("DB connection created successfully: %s%n", dbProps.getProperty("url"));
+
+            // 3. Create and execute db statements
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO products (code, name, description, price, unit) VALUES (?, ?, ?, ?, ?)");
+            int numInserts = 0;
+            for(Product p: products){
+                ps.setString(1, p.getCode());
+                ps.setString(2, p.getName());
+                ps.setString(3, p.getDescription());
+                ps.setDouble(4, p.getPrice());
+                ps.setInt(5, p.getUnit().ordinal());
+                numInserts += ps.executeUpdate();
+            }
+            System.out.printf("%d records inserted successfully.", numInserts);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
