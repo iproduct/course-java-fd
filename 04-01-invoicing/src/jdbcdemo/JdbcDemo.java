884c7a4636e853cd7c4d5d5936acf5db114d5e15
Trayan Iliev <office@iproduct.org>
2021-03-18 14:19:52 +0200

    products query demo added

--- 04-01-invoicing/src/jdbcdemo/JdbcDemo.java
+++ 04-01-invoicing/src/jdbcdemo/JdbcDemo.java
@@ -1,15 +1,15 @@
 package jdbcdemo;
 
+import invoicing.InvoicingApp;
 import invoicing.model.Product;
 import invoicing.model.Unit;
+import invoicing.util.PrintUtil;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -27,6 +27,9 @@ public class JdbcDemo {
                     0.72, Unit.M)
     );
 
+    public static final String INSERT_PRODUCT_SQL = "INSERT INTO products (code, name, description, price, unit) VALUES (?, ?, ?, ?, ?)";
+    public static final String SELECT_ALL_PRODUCTS_SQL = "SELECT * FROM products";
+
     public static void main(String[] args) throws IOException {
         String dbConfigPath = JdbcDemo.class.getClassLoader().getResource("db.properties").getPath();
         Properties dbProps = new Properties();
@@ -42,23 +45,42 @@ public class JdbcDemo {
         System.out.println("Program completed successfully.");
 
         // 2. Connect to DB
-        try {
-            Connection con = DriverManager.getConnection(dbProps.getProperty("url"), dbProps);
+        try (Connection con = DriverManager.getConnection(dbProps.getProperty("url"), dbProps)){
             System.out.printf("DB connection created successfully: %s%n", dbProps.getProperty("url"));
 
             // 3. Create and execute db statements
-            PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO products (code, name, description, price, unit) VALUES (?, ?, ?, ?, ?)");
-//            int numInserts = 0;
-//            for(Product p: products){
-//                ps.setString(1, p.getCode());
-//                ps.setString(2, p.getName());
-//                ps.setString(3, p.getDescription());
-//                ps.setDouble(4, p.getPrice());
-//                ps.setInt(5, p.getUnit().ordinal());
-//                numInserts += ps.executeUpdate();
+            int numInserts;
+//            try (PreparedStatement ps = con.prepareStatement(INSERT_PRODUCT_SQL)) {
+//                numInserts = 0;
+//                for (Product p : products) {
+//                    ps.setString(1, p.getCode());
+//                    ps.setString(2, p.getName());
+//                    ps.setString(3, p.getDescription());
+//                    ps.setDouble(4, p.getPrice());
+//                    ps.setInt(5, p.getUnit().ordinal());
+//                    numInserts += ps.executeUpdate();
+//                }
 //            }
 //            System.out.printf("%d records inserted successfully.", numInserts);
+
+            try (Statement ps = con.createStatement()) {
+                ResultSet rs = ps.executeQuery(SELECT_ALL_PRODUCTS_SQL);
+                List<Product> products = new ArrayList<>();
+                while(rs.next()) {
+                    Product p = new Product(
+                            rs.getLong("id"),
+                            rs.getString("code"),
+                            rs.getString("name"),
+                            rs.getString("description"),
+                            rs.getDouble("price"),
+                            Unit.values()[rs.getInt("unit")]
+                    );
+                    products.add(p);
+                }
+                System.out.println(
+                        PrintUtil.formatTable(InvoicingApp.PRODUCT_COLUMNS, products, "Products List"));
+            }
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
