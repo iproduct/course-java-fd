5259877773f15bc4f1062e781d7c4ace905db77c
Trayan Iliev <office@iproduct.org>
2021-03-18 10:56:30 +0200

    MenuUtil class - show menu completed

--- 04-01-invoicing/src/invoicing/util/MenuUtil.java
+++ 04-01-invoicing/src/invoicing/util/MenuUtil.java
@@ -1,17 +1,32 @@
 package invoicing.util;
 
 import invoicing.model.Role;
+import invoicing.view.Command;
 import invoicing.view.MenuItem;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static invoicing.model.Role.ADMIN;
+import static invoicing.util.FieldType.INTEGER;
+
 public class MenuUtil {
-    public static void showMenu(Map<Integer, MenuItem> menuItems) {
+    public static void showMenu(Map<Integer, MenuItem> menuItems, Map<MenuItem, Command> commands) {
         boolean exit = false;
+        int choice;
+        FieldConfig intFieldConfig = new FieldConfig(null, "Choose an option", INTEGER, 2);
         do {
-            System.out.println();
+            System.out.println(formatMenu(menuItems, ADMIN));
+            choice = InputUtil.inputLong(intFieldConfig).intValue();
+            MenuItem chosenItem = menuItems.get(choice);
+            if(chosenItem != null) {
+                Command chosenCommand = commands.get(chosenItem);
+                System.out.println();
+                System.out.println(chosenCommand.execute());
+            } else {
+                System.out.println("Invalid chice. Try again");
+            }
         } while (!exit);
     }
 
